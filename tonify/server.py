a970feb08e9c407019c980c4dc220e7f7cf29f36
Daniel Hails <daniel@hails.info>
2021-11-13 18:46:35 +0000

    wip

---
+++ tonify/server.py
@@ -0,0 +1,64 @@
+import os
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, jsonify
+
+API_VERSION_NUMBER = '1.0'
+API_VERSION_LABEL = 'v1'
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = {'json'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/echo', methods=['POST'])
+def hello():
+   return jsonify(request.json)
+
+@app.route('/analyse', methods=['POST'])
+def analyse_messages():
+    messages = request.json
+
+    print(messages["name"])
+    # print(messages.name)
+
+    return f"Hello {messages['name']}"
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print(url_for('download_file', name=filename))
+            return redirect(url_for('download_file', name=filename))
+    
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/upload/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
