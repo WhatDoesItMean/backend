6b1b3ee6e15896ac1d7c5a76cf5c2239c09b431c
MuhenderRaj <rkmr02@gmail.com>
2021-11-13 13:41:12 -0500

    clean slate

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "hello world"
+
+@app.route('/send', methods=['POST'])
+def send_messages():
+    messages = request.json['messages']
+    
+    result = run_model(messages)
+    
+    return jsonify(result)
+
+def run_model(messages):
+    pass
